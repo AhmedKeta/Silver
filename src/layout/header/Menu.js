df19c91afcbcf93038aa9b9db93cad713e258ec9
AhmedKeta <ahmedketa12@gmail.com>
2023-08-15 16:14:32 +0300

    ReCAPTCHA + some edit in meta component + rsponsive in project slider in home + adding social media in mobile menu

--- src/layout/header/Menu.js
+++ src/layout/header/Menu.js
@@ -36,8 +36,7 @@ const MobileMenu = () => {
             type="button"
             className="navbar-toggle"
             eventKey="collapse"
-            data-bs-target=".navbar-collapse"
-          >
+            data-bs-target=".navbar-collapse">
             <span className="icon-bar" />
             <span className="icon-bar" />
             <span className="icon-bar" />
@@ -45,8 +44,7 @@ const MobileMenu = () => {
         </div>
         <Accordion.Collapse
           eventKey="collapse"
-          className="navbar-collapse clearfix"
-        >
+          className="navbar-collapse clearfix">
           <ul className="navigation clearfix">
             {/* Home */}
             <li className="dropdown">
@@ -175,6 +173,31 @@ const MobileMenu = () => {
               </ul>
               <div className="dropdown-btn" onClick={() => active("pages")}>
                 <span className="fas fa-chevron-down" />
+              </div>
+            </li>
+            <li>
+              <div className=" social-style-three" id="mobile-social">
+                <a href="https://www.facebook.com/people/Silver-Pyramids-Inc/100094335560472/">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a href="https://x.com/silverpyramids1">
+                  <i className="fab fa-x-twitter" />
+                </a>
+                <a href="https://www.instagram.com/silverpyramids/">
+                  <i className="fab fa-instagram" />
+                </a>
+                <a href="https://www.linkedin.com/company/silver-pyramids-inc./mycompany/?viewAsMember=true">
+                  <i className="fab fa-linkedin-in" />
+                </a>
+                <a href="https://www.tiktok.com/@silver.pyramids?ug_source=op.auth&ug_term=Linktr.ee&utm_source=awyc6vc625ejxp86&utm_campaign=tt4d_profile_link&_r=1">
+                  <i className="fab fa-tiktok" />
+                </a>
+                <a href="https://web.snapchat.com/?ref=snapchat_dot_com_login_cta">
+                  <i className="fab fa-snapchat" />
+                </a>
+                <a href="https://www.youtube.com/@SilverPyramids1">
+                  <i className="fab fa-youtube" />
+                </a>
               </div>
             </li>
           </ul>
